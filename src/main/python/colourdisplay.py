1f8f818eb34d525239b79ea8713c8113a9a1ea4e
C. E. Sharp <sharpc@uk.ibm.com>
2018-04-28 15:09:12 +0100

    clean some codacy issues

--- src/main/python/colourdisplay.py
+++ src/main/python/colourdisplay.py
@@ -6,17 +6,17 @@ class ColourDisplay(Display):
     DEFAULT = Display.ESC + str(Colour.WHITE) + "m"
     DEFAULT += Display.ESC + str(Colour.BLACK_BG) + "m"
 
-    def refresh(self, video_output, colour_out):
+    def refresh(self, video_output, colour_out=None):
         self.output = ""
         self.write_stream(Display.CLR)
         self.write_stream(ColourDisplay.DEFAULT)
-        for i in range(len(video_output)):
-            if colour_out[i] > 0:
-                self.write_stream(Display.ESC + str(colour_out[i]) + "m")
-                self.write_stream(video_output[i])
+        for index, video_data in enumerate(video_output):
+            if colour_out[index] > 0:
+                self.write_stream(Display.ESC + str(colour_out[index]) + "m")
+                self.write_stream(video_data)
                 self.write_stream(Display.RST)
                 self.write_stream(ColourDisplay.DEFAULT)
             else:
-                self.write_stream(video_output[i])
+                self.write_stream(video_data)
         self.write_stream(Display.RST)
         self.write_stream("\n")
