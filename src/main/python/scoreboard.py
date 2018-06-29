d77fdfa86377bc8fc1158f8d7a7241f0d0747349
C. E. Sharp <sharpc@uk.ibm.com>
2018-06-29 08:26:09 +0100

    fix package name for openapi

--- src/main/python/scoreboard.py
+++ src/main/python/scoreboard.py
@@ -1,19 +1,19 @@
-import swagger_client
-from swagger_client.rest import ApiException
+import openapi_client
+from openapi_client.rest import ApiException
 
 
 class Scoreboard(object):
 
     def __init__(self, scoreboard_url, player):
-        config = swagger_client.Configuration()
+        config = openapi_client.Configuration()
         config.host = scoreboard_url
-        api_client = swagger_client.ApiClient(config)
-        self.api_instance = swagger_client.ScoreboardApi(api_client)
+        api_client = openapi_client.ApiClient(config)
+        self.api_instance = openapi_client.ScoreboardApi(api_client)
         self.player = player
 
     def post_score(self, score):
         try:
-            body = swagger_client.Score(self.player, score)
+            body = openapi_client.Score(self.player, score)
             return self.api_instance.add_score(body)
         except ApiException as e:
             print("Exception when calling ScoreboardApi->add_score: %s\n" % e)
