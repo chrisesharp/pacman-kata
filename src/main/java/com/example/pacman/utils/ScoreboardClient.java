f3ea1d0eab930fc7911c6cd3e43f1953f5554343
C. E. Sharp <sharpc@uk.ibm.com>
2018-07-10 08:54:28 +0100

    Add beginning of JWT support

--- src/main/java/com/example/pacman/utils/ScoreboardClient.java
+++ src/main/java/com/example/pacman/utils/ScoreboardClient.java
@@ -4,9 +4,11 @@ import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
 import org.openapitools.client.model.Score;
 import org.openapitools.client.api.ScoreboardApi;
+import org.openapitools.client.auth.ApiKeyAuth;
 import org.apache.log4j.Logger;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ScoreboardClient {
   private static final Logger log = Logger.getLogger(ScoreboardClient.class);
@@ -25,6 +27,9 @@ public class ScoreboardClient {
   
   public void setApiClient(ApiClient client) {
     client.setBasePath(scoreboardURL);
+    ApiKeyAuth authScheme = (ApiKeyAuth) client.getAuthentication("scoreboardService_auth_Bearer");
+    authScheme.setApiKeyPrefix("Bearer");
+    authScheme.setApiKey("JWT_goes_here");
     scoreboard.setApiClient(client);
   }
   
